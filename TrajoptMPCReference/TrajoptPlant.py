6e16f291c5eca9de3ebfbc4bcc9f728bb6f19fc2
Kevin Qiu <kzqiu01@gmail.com>
2024-04-08 13:54:26 -0400

    Added QuadRotor plant to TrajoptPlant.py

--- TrajoptMPCReference/TrajoptPlant.py
+++ TrajoptMPCReference/TrajoptPlant.py
@@ -323,3 +323,196 @@ class URDFPlant(TrajoptPlant):
 
 	def get_num_cntrl(self):
 		return self.robot.get_num_cntrl()
+
+
+class QuadrotorPlant(TrajoptPlant):
+    def _init_(self, integrator_type = 0, options = {}):
+        super()._init_(integrator_type, options)
+
+        # base parameters
+        self.g = 9.81 # m / s^2
+        self.m = 0.468 # kg
+        self.l = 0.225 # m, distance from center of mass to rotor
+        self.k = 2.98e-6 # lift constant
+        self.b = 1.14e-7 # drag constant
+
+        # moments of inertia
+        self.Ixx = 4.856e-3 # kg * m^2
+        self.Iyy = 4.856e-3 # kg * m^2
+        self.Izz = 8.801e-3 # kg * m^2
+        self.Im = 3.3357e-5 # kg * m^2
+        # drag coefficients
+        self.Ax = 0.25 # kg / s
+        self.Ay = 0.25 # kg / s
+        self.Az = 0.25 # kg / s
+
+    def forward_dynamics(self, x: np.ndarray, u: np.ndarray):
+        # Assuming x = [px, py, pz, phi, theta, psi, vx, vy, vz, p, q, r]
+        phi, theta, psi = x[3:6]
+        p, q, r = x[9:12]
+
+        # Control inputs u = [w1, w2, w3, w4]
+        fi = self.k * u**2
+        T = np.sum(fi)
+        # tau_phi = self.l * self.k * (u[2]*2 - u[0]*2)
+        # tau_theta = self.l * self.k * (u[3]*2 - u[1]*2)
+        # tau_psi = self.b * (u[1]*2 - u[0]*2 + u[3]*2 - u[2]*2)
+        tau_phi = self.l * self.k * (-u[1] ** 2 + u[3] ** 2)
+        tau_theta = self.l * self.k * (-u[0] ** 2 + u[2] ** 2)
+        tau_psi = self.b * np.sum(u ** 2)
+
+        # Assemble torques into vector
+        tau = np.array([tau_phi, tau_theta, tau_psi])
+
+        C_phi = np.cos(phi)
+        S_phi = np.sin(phi)
+        T_phi = np.tan(phi)
+        C_theta = np.cos(theta)
+        S_theta = np.sin(theta)
+        T_theta = np.tan(theta)
+        C_psi = np.cos(psi)
+        S_psi = np.sin(psi)
+        T_psi = np.tan(psi)
+
+        # Compute rotation matrix R from body to inertial frame
+        R = np.array([
+            [C_psi * C_theta, C_psi * S_theta * S_phi - S_psi * C_phi, C_psi * S_theta * C_phi + S_psi * S_phi],
+            [S_psi * C_theta, S_psi * S_theta * S_phi - C_psi * C_phi, S_psi * S_theta * C_phi - C_psi * S_theta],
+            [-S_theta, C_theta * S_phi, C_theta * C_phi]
+        ])
+
+        # Translational dynamics
+        TB = np.array([0, 0, T])  # Thrust in body frame
+        translational_vec = np.array([
+            [C_psi*S_theta*C_phi+S_psi*S_phi],
+            [S_psi*S_theta*C_phi-C_psi*S_phi],
+            [C_theta*C_phi]
+        ])
+        tranlation_acc = (
+            -self.g * np.array([[0],[0],[1]]) 
+            + (T / self.m) @ translational_vec 
+            # - (1 / self.m ) * np.diag([self.Ax, self.Ay, self.Az]) @ [x0dot, x1dot, x2dot]
+        )
+
+        # Rotational dynamics
+        W_n_inv = np.array([
+            [1, S_phi*T_theta, C_phi*T_theta],
+            [0, C_phi, -S_phi],
+            [0, S_phi/C_theta, C_phi/C_theta]
+        ])
+        n_dot = W_n_inv @ np.array([p,q,r])
+        phi_dot = n_dot[0]
+        theta_dot = n_dot[1]
+        psi_dot = n_dot[2]
+        w_gamma = u[0]-u[1]+u[2]-u[3]
+        Ir = np.sqrt(self.Ixx ** 2 + self.Iyy ** 2 + self.Izz ** 2) # didn't find what is Ir assumed it norm of total moment
+        # Tb = np.array[[
+        #     [self.l*self.k*(-u[1]*2+u[3]*2)]
+        #     [self.l*self.k*(-u[1]*2+u[2]*2)]
+        #     [self.b*((u[0])*2+u[1]*2+u[2]*2+u[3]*2)]
+        # ]]
+        v_dot = np.array([
+            [(self.Iyy-self.Izz)*q*r/self.Ixx]
+            [(self.Izz-self.Ixx)*p*r/self.Iyy]
+            [(self.Ixx-self.Iyy)*p*q/self.Izz]
+        ]) - w_gamma*Ir * np.array([
+            [q/self.Ixx]
+            [-p/self.Iyy]
+            [0]
+        ]) + np.array([
+            [tau_phi/self.Ixx]
+            [tau_theta/self.Iyy]
+            [tau_psi/self.Izz]
+        ])
+
+        rotational_acc_matrix = np.array([
+            [0, phi_dot*C_phi*T_theta+theta_dot*S_phi/(C_theta**2), -phi_dot*S_phi*C_theta+theta_dot*C_phi/(C_theta**2)],
+            [0,-phi_dot*S_phi,-phi_dot*C_phi],
+            [0, phi_dot*C_phi/C_theta+T_theta*phi_dot*S_phi/(C_theta), -phi_dot*S_phi/C_theta+theta_dot*C_phi*T_theta/(C_theta)]
+        ])
+        v = np.array([
+            [p]
+            [q]
+            [r]
+        ])
+
+        # Compute rotational accelerations in inertial frame
+        rotational_acc =  rotational_acc_matrix @ v + W_n_inv @ v_dot
+        acceleration = np.concatenate((tranlation_acc, rotational_acc))
+        return acceleration
+
+    def forward_dynamics_gradient(self, x: np.ndarray, u: np.ndarray):
+        phi, theta, psi = x[3:6]
+        p, q, r = x[9:12]
+
+        fi = self.k * u**2
+        T = np.sum(fi)
+
+        C_phi = np.cos(phi)
+        S_phi = np.sin(phi)
+        T_phi = np.tan(phi)
+        C_theta = np.cos(theta)
+        S_theta = np.sin(theta)
+        T_theta = np.tan(theta)
+        C_psi = np.cos(psi)
+        S_psi = np.sin(psi)
+        T_psi = np.tan(psi)
+
+        # computing translational rotatation component gradients
+
+        ang_trans_grad = T / self.m * np.array([
+            [-C_psi * S_theta * S_phi + S_psi * C_phi, C_psi * C_theta * C_phi, - S_psi * S_theta * C_phi + C_psi * S_phi],
+            [-S_psi * S_theta * S_phi - C_psi * C_phi, S_psi * C_theta * C_phi, C_psi * S_theta * C_phi + S_psi * S_phi],
+            [-S_phi * C_theta, -S_theta * C_phi, 0]
+        ])
+
+        translational_vec = np.array([
+            [C_psi*S_theta*C_phi+S_psi*S_phi],
+            [S_psi*S_theta*C_phi-C_psi*S_phi],
+            [C_theta*C_phi]
+        ])
+
+        torque_grad = 2 * self.k * u
+
+        trans_control_grad = 1 / self.m * translational_vec @ torque_grad
+
+        trans_grad = np.hstack((np.zeros(3, 3), ang_trans_grad, np.zeros(3, 6), trans_control_grad))
+
+        # computing rotational component gradients, using central finite difference approximation
+        # perturb by some h then grad = f(x + h) - f(x - h) / 2h
+
+        h = 0.01
+        
+        angular_grad = np.hstack([self.central_diff_approx(x, u, i, h) for i in range(3, 6)])
+        angular_vel_control_grad = np.hstack([self.central_diff_approx(x, u, i, h) for i in range(9, 16)])
+
+        rot_grad = np.hstack((np.zeros(3, 3), angular_grad, np.zeros(3, 3), angular_vel_control_grad))
+
+        return np.hstack((trans_grad, rot_grad))
+
+    def central_diff_approx(self, x: np.ndarray, u: np.ndarray, i: int, h: float):
+        if i < x.shape[0]:
+            xa = x
+            xb = x
+            xa[i] += h
+            xb[i] -= h
+
+            return (self.forward_dynamics(xa, u) - self.forward_dynamics(xb, u)) / (2 * h)
+            
+        i -= x.shape[0]
+        ua = u
+        ub = u
+        ua[i] += h
+        ub[i] -= h
+
+        return (self.forward_dynamics(x, ua) - self.forward_dynamics(x, ub)) / (2 * h)
+
+
+    def get_num_pos(self):
+        return 6
+
+    def get_num_vel(self):
+        return 6
+
+    def get_num_cntrl(self):
+        return 4
